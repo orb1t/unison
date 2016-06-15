7fa74d3c98e8d84a5550ad930d81e9fdf58fac89
Tim McIver <tim@timmciver.com>
2016-06-14 22:35:16 -0400

    Changed the type of the `evaluate` function so that a `Pool` can manage the process IDs rather than let the client do it.

--- node/src/Unison/Runtime/ProcessPool.hs
+++ node/src/Unison/Runtime/ProcessPool.hs
@@ -11,11 +11,14 @@ data Err = TimeExceeded | SpaceExceeded | Killed | InsufficientProcesses
 type ID = Int
 
 data Pool a b = Pool {
-  -- | Evaluate the thunk in a separate process, with the given budget. 
+  -- | Evaluate the thunk in a separate process, with the given budget.
+  -- The outer IO completes immediately yielding a tuple containing the ID
+  -- of the process being used for evaluation and another IO which, when
+  -- it completes, yields the result of the evaluation.
   -- If there is no available process for that `Budget`, create a new one, 
   -- unless that would exceed the `MaxProcesses` bound, in which case
   -- fail fast with `Left InsufficientProcesses`.
-  evaluate :: Budget -> ID -> a -> IO (Either Err b),
+  evaluate :: Budget -> a -> IO (ID, IO (Either Err b)),
 
   -- | Forcibly kill the process associated with an ID. Any prior `evaluate` for
   -- that `ID` should complete with `Left Killed`.
